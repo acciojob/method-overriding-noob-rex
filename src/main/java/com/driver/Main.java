772d4bd09aa18555e8e7d2c277544ac464f4337d
pr@bin <99886121+noob-rex@users.noreply.github.com>
2024-09-15 19:22:15 +0530

    Update Main.java

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,9 +1,11 @@
 package com.driver;
+
 class A {
     public String meth() {
         return "Invoking method from class A";
     }
 }
+
 class B extends A {
     @Override
     public String meth() {
@@ -14,7 +16,8 @@ class B extends A {
 public class Main {
     public static void main(String[] args) {
         B obj = new B();
-        System.out.println(obj.meth()); 
-        System.out.println(obj.meth()); 
+
+
+        System.out.println(obj.meth());
     }
 }
