30c3baa01b026d8d4737bb103159d9ac154874c8
pr@bin <99886121+noob-rex@users.noreply.github.com>
2024-09-15 21:00:20 +0530

    Update Main.java

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -17,7 +17,7 @@ public class Main {
     }
 
     public static void main(String[] args) {
-        B obj = new B();
+        B obj = new A();
         obj.meth();
         B obj1 = new B();
         obj1.meth();
