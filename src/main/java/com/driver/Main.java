491b8349ca6c28a9d2d3906f8ba6af238c85c22d
pr@bin <99886121+noob-rex@users.noreply.github.com>
2024-09-15 19:35:20 +0530

    Update Main.java

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -7,6 +7,10 @@ class A {
 }
 
 class B extends A {
+    public String callSuperMeth() {
+        return super.meth();
+    }
+    
     @Override
     public String meth() {
         return "Method is overridden in Extended class B";
@@ -15,9 +19,11 @@ class B extends A {
 
 public class Main {
     public static void main(String[] args) {
+        // Task 3: Create an object of class B and call method meth of class A with it
         B obj = new B();
-
-
-        System.out.println(obj.meth());
+        System.out.println("Calling method of class A with B object: " + obj.callSuperMeth());
+        
+        // Task 5: Call the overridden method from object of class B
+        System.out.println("Calling overridden method with B object: " + obj.meth());
     }
 }
