18655a850e2845fd9858f4af3304df0ac99c59e4
pr@bin <99886121+noob-rex@users.noreply.github.com>
2024-09-15 20:53:03 +0530

    Update Main.java

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,21 +1,25 @@
 package com.driver;
 
-class A {
-    public String meth() {
-        return "Invoking method from class A";
-    }
-}
-
-class B extends A {
-    @Override
-    public String meth() {
-        return "Method is overridden in Extended class B";
-    }
-}
-
 public class Main {
+
+    public static class A {
+        public String meth()
+        {
+            return "Invoking method from class A";
+        }
+    }
+
+    public static class B extends A {
+        public String meth()
+        {
+            return "Method is overridden in Extended class B";
+        }
+    }
+
     public static void main(String[] args) {
         B obj = new B();
-        System.out.println(obj.meth());
+        obj.meth();
+        B obj1 = new B();
+        obj1.meth();
     }
 }
